21f7ba52963ed5d5ca3f16e372e6a74a657dc631
Robert Safin <robertsafin98@gmail.com>
2025-06-17 07:23:19 +0800

    stomp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal("Error: %w", err)
+	}
+	defer connection.Close()
+	fmt.Println("Connected to RabbitMQ")
 	fmt.Println("Starting Peril server...")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down")
 }
